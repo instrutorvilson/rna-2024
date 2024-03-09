cb8a0265fe6126cbb3c7fd30a003136c09b1fcb3
Aluno ProWay <aluno@proway.treina>
2024-03-09 15:23:24 -0300

    Navegação tab-bottom

---
+++ app-bottom-nav/src/componentes/about.js
@@ -0,0 +1,8 @@
+import {View, Text} from 'react-native'
+export default function About(){
+    return(
+        <View>
+            <Text>about</Text>
+        </View>
+    )
+}
---
+++ app-bottom-nav/src/componentes/consulta.js
@@ -0,0 +1,8 @@
+import {View, Text} from 'react-native'
+export default function Consulta(){
+    return(
+        <View>
+            <Text>consulta</Text>
+        </View>
+    )
+}
