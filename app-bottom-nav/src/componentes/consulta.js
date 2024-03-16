0fd125d3618b76136b2de862cba940462e58b10c
Aluno ProWay <aluno@proway.treina>
2024-03-16 15:40:18 -0300

    Ajustes de branch

--- app-bottom-nav/src/componentes/consulta.js
+++
@@ -1,8 +0,0 @@
-import {View, Text} from 'react-native'
-export default function Consulta(){
-    return(
-        <View>
-            <Text>consulta</Text>
-        </View>
-    )
-}
--- app-gestor/src/componentes/administrativo/configuracao.js
+++
@@ -1,8 +0,0 @@
-import {View, Text} from 'react-native'
-export default function Configuracao(){
-    return(
-        <View>
-            <Text>Configuracao</Text>
-        </View>
-    )
-}
