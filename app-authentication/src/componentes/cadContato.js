4128d4f6a780726fcb99bcd905ca58940e46bc7d
Aluno ProWay <aluno@proway.treina>
2024-03-23 14:46:21 -0300

    Salvar dados de contato no google cloud firestore

--- app-authentication/src/componentes/cadContato.js
+++ app-authentication/src/componentes/cadContato.js
@@ -1,30 +1,134 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text, Button } from "react-native"
+import { useEffect, useRef, useState } from "react";
+import { StyleSheet, View, Text, Button, TextInput } from "react-native"
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { getFirestore, collection, addDoc } from "firebase/firestore"
+import app from '../configuracao/firebaseConfig'
 
-export default function CadContato({navigation}){
-    useEffect(()=> {verificaUserLogado()} ,[])
+import Toast from 'react-native-toast-message'
 
-    async function verificaUserLogado(){
+export default function CadContato({ navigation }) {
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [fone, setFone] = useState('')
+
+    const nomeRef = useRef('')
+    const emailRef = useRef('')
+    const foneRef = useRef('')
+
+    //pegar a base de dados
+    const db = getFirestore(app)
+    useEffect(() => { verificaUserLogado() }, [])
+
+    async function verificaUserLogado() {
         let userLogado = await AsyncStorage.getItem('userLogado')
-        if(userLogado == null){
+        if (userLogado == null) {
             navigation.navigate('login')
         }
     }
 
-    return(
+    async function handleGravar() {
+       if(!validarDados()){
+          return
+       }
+        //grava a coleçaõ no google cloud
+       await  addDoc(collection(db, 'contatos'), { nome, email, fone })
+       Toast.show({
+        type: 'success',
+        text1: 'Parabéns',
+        text2: 'O contato foi cadastrado com sucesso'
+    })
+    }
+
+    function validarDados(){
+        if(nome == ''){
+            Toast.show({
+                type: 'error',
+                text1: 'Cuidado',
+                text2: 'O nome deve ser informado'
+            })
+            nomeRef.current.focus()
+            return false
+        }
+        //valida se email foi informado
+        if(email == ''){
+            Toast.show({
+                type: 'error',
+                text1: 'Cuidado',
+                text2: 'O email deve ser informado'
+            })
+            emailRef.current.focus()
+            return false
+        }
+        //valida se fone foi informado
+        if(fone == ''){
+            Toast.show({
+                type: 'error',
+                text1: 'Cuidado',
+                text2: 'O fone deve ser informado'
+            })
+            foneRef.current.focus()
+            return false
+        }
+
+        //se não aconteceu nenhum problema retorna true
+        return true
+    }
+
+    return (
         <View style={styles.container}>
-            <Text style={{textTransform: 'uppercase'}}>Cadastro contato</Text> 
-            
+            <Text style={{ textTransform: 'uppercase', textAlign: 'center' }}>Cadastro contato</Text>
+            <Text style={styles.rotulo}>Informe nome</Text>
+            <TextInput
+                style={styles.input}
+                defaultValue={nome}
+                onChangeText={txt => setNome(txt)}
+                placeholder="Ex: Maria da Silva"
+                ref={nomeRef}
+            />
+
+            <Text style={styles.rotulo}>Informe email</Text>
+            <TextInput
+                style={styles.input}
+                defaultValue={email}
+                onChangeText={txt => setEmail(txt)}
+                placeholder="Ex: maria@gmail.com"
+                ref={emailRef}
+            />
+
+            <Text style={styles.rotulo}>Informe fone</Text>
+            <TextInput
+                style={styles.input}
+                defaultValue={fone}
+                onChangeText={txt => setFone(txt)}
+                placeholder="Ex: (47)9090-7080"
+                ref={foneRef}
+            />
+            <Button
+                title='gravar'
+                onPress={handleGravar}
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+    input: {
+        marginVertical: '5px',
+        padding: '10px',
+        borderBottomWidth: '1px'
+
     },
-  });
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'left',
+        justifyContent: 'center',
+        padding: '100px'
+    },
+    rotulo: {
+        fontWeight: '700',
+        fontSize: '14px',
+        fontFamily: 'verdana',
+        marginTop: '10px'
+    }
+});
