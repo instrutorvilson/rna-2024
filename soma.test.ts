0a61acb4751cf2777d6296d239b3237e82c85406
instrutorvilsonmoro@gmail.com <instrutorvilsonmoro@gmail.com>
2024-08-03 17:58:23 -0300

    Configurando testes unitários que lança exceção

--- soma.test.ts
+++ soma.test.ts
@@ -1,9 +1,18 @@
-import { multiplicacao, soma } from "./soma";
+import { divide, multiplicacao, soma } from "./soma";
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(soma(1, 2)).toBe(3);
+    expect(soma(1, 2)).toBe(3);
 });
 
 test('Multiplicar 10 + 3 to equal 30', () => {
     expect(multiplicacao(10, 3)).toBe(30);
-  });
+});
+
+test('Dividir 10 / 2 to equal 5', () => {
+    expect(divide(10, 2)).toBe(5);
+});
+
+test('Dividir 10 / 0 should throw Error', () => {
+    expect(() => divide(10, 0)).toThrow('Não é possivel dividir por zero');
+});
+
--- soma.ts
+++ soma.ts
@@ -10,6 +10,6 @@ export function divide(n1:number, n2:number): number{
     if(n2 == 0){
         throw new Error("Não é possivel dividir por zero")
     }
-    return n1 + n2
+    return n1 / n2
 }
 
