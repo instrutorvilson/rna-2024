c08c927436859830ee3fbfae7166762f45a58d59
instrutorvilsonmoro@gmail.com <instrutorvilsonmoro@gmail.com>
2024-08-03 17:14:25 -0300

    adicionado estilos

--- card.tsx
+++ card.tsx
@@ -1,24 +1,25 @@
-import { Text, View, Pressable } from "react-native";
+import { Text, View, Pressable, StyleSheet } from "react-native";
 import { TCard } from "./types";
 import { Swipeable } from "react-native-gesture-handler";
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
 
 export default function Card(props: TCard) {
     const renderRightActions = () => (
-        <View>
+        <View style={styles.containerDelete}> 
             <Pressable 
-               style={{backgroundColor:'red'}}
+               style={styles.botaoDelete}
                onPress={props.onDelete}>
-                <Text>Deletar</Text>
+                <Text style={{color:'white'}}>Delete</Text>
             </Pressable>
         </View>
     )
-
 
     return (
         <Swipeable
             renderRightActions={renderRightActions}
         >
-            <View style={{ backgroundColor: '#aaa', padding: 10, marginVertical: 10 }}>
+            <View style={styles.card}>
                 <Text style={{ fontWeight: 700 }}>
                     ID: <Text style={{ fontWeight: 100 }}>{props.id}</Text>
                 </Text>
@@ -29,3 +30,21 @@ export default function Card(props: TCard) {
         </Swipeable>
     )
 }
+
+const styles = StyleSheet.create({
+    card: {
+      padding: 20,
+      margin: 10,
+      backgroundColor: 'lightblue',
+      borderRadius: 5
+    },
+    containerDelete:{
+      
+      justifyContent: 'center' 
+    },
+    botaoDelete:{
+       backgroundColor: 'red',
+       borderRadius: 50,
+       padding: 10
+    }
+  });
